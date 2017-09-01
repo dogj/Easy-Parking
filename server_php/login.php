e032c2a3ee048681bd825ef88132d2c05f07b6ad
Bruce Li <zilong.li@it.weltec.ac.nz>
2017-09-01 14:12:51 +1200

    delete anotation
    delete anotation

--- server_php/login.php
+++ server_php/login.php
@@ -1,11 +1,11 @@
 <?php
 include_once 'database_conn.inc.php';
 
-// $username = $_POST['username'];
-// $password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$username = "zilong";
-$password = "123";
+// $username = "zilong";
+// $password = "123";
 
 $sql = "SELECT * FROM user WHERE username ='$username' AND password = '$password'";
 $result = $conn->query($sql) or die (mysqli_connect_error());  
