26de8de781a2490e35ce9b9dec3c54b8510b04a1
aimee <yhchixigua@gmail.com>
2017-09-14 10:11:44 +1200

    add junit test for sunday class

---
+++ app/src/test/java/com/jiangxin/packing/SundayTest.java
@@ -0,0 +1,26 @@
+package com.jiangxin.packing;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aimee on 14/09/17.
+ */
+public class SundayTest {
+    @Test
+    public void isSun() throws Exception {
+
+        Boolean result = false;
+        Boolean expect = Sunday.isSun();
+
+        assertEquals(result,expect);
+
+
+
+    }
+
+}
