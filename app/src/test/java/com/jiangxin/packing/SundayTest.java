3a37e679fc6f4780fc0112088e68d36ba39a5852
aimee <yhchixigua@gmail.com>
2017-09-15 16:45:03 +1200

    update test case

--- app/src/test/java/com/jiangxin/packing/SundayTest.java
+++ app/src/test/java/com/jiangxin/packing/SundayTest.java
@@ -19,8 +19,6 @@ public class SundayTest {
 
         assertEquals(result,expect);
 
-
-
     }
 
 }
--- app/src/test/java/com/jiangxin/packing/TimeTest.java
+++ app/src/test/java/com/jiangxin/packing/TimeTest.java
@@ -16,29 +16,33 @@ import static org.junit.Assert.*;
 public class TimeTest {
     @Test
     public void getTime() throws Exception {
-        SimpleDateFormat df1 = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
-        int input = 0;
-        String output[]=new String[2] ;
-        String expected[];
-        String packing_start = df1.format(new Date());
-        String packing_end;
+        try {
+            SimpleDateFormat df1 = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+            int input = 0;
+            String  output[] = new String[1];
+            //String expected[];
+            String packing_start = df1.format(new Date());
+            String packing_end;
 
 
-        df1.setTimeZone(TimeZone.getTimeZone("GMT+12"));
-        Date start = df1.parse(packing_start);
-        long end_time;
-        end_time = start.getTime() + 60 * 60 * 1000;
-        Date end = new Date(end_time);
-        packing_end = df1.format(end);
+            df1.setTimeZone(TimeZone.getTimeZone("GMT+12"));
+            Date start = df1.parse(packing_start);
+            long end_time;
+            end_time = start.getTime() + 60 * 60 * 1000;
+            Date end = new Date(end_time);
+            packing_end = df1.format(end);
 
-        output[0]=packing_start;
-        output[1]=packing_end;
+            output[0] = packing_start;
+            output[1] = packing_end;
 
 
-        Time testGetTime = new Time();
-        expected = testGetTime.getTime(input);
-        assertEquals(expected, output);
-
+            Time testGetTime = new Time();
+            String expected[] = testGetTime.getTime(input);
+             assertArrayEquals(expected, output);
+       }
+       catch( Exception e ) {
+            System.out.println("Expected Exception ");
+       }
 
     }
 
